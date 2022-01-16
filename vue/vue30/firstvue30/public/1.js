44fab8c624c709f3dc56f66d5970f046f200fe13
unknown <2292082032.@qq.com>
2022-01-16 10:45:10 +0800

    删除

--- vue/vue30/firstvue30/public/1.js
+++
@@ -1,4 +0,0 @@
-var a=1;
-var t=a;
-var a=3;
-console.log(t);
--- vue/vue30/firstvue30/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './components/22.vue'
-import router from './router'
-import store from './store'
-import ElementPlus from 'element-plus'
-import 'element-plus/theme-chalk/index.css'
-
-createApp(App).use(ElementPlus).use(store).use(router).mount('#app')
